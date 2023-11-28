ac92f2190baac06a231f36fcd2a0c6df16c5457b
GwenafyMrg <gwenafy@gmail.com>
2023-11-28 13:07:58 +0100

    Backend
    Ajout de la première partie backend de la page web. Flask lié au HTML/CSS. Navigation sur le site Web via Flask.

---
+++ App.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def bienvenue():
+    return render_template("accueil.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/create_account")
+def create_account():
+    return render_template("create_account.html")
+
+@app.route("/traitement", methods=["POST"])
+def traitement():
+    donnees = request.form
+    user = donnees['username']
+    pw = donnees['password']
+    print(user,pw)
+    if user == "gwen" and pw == "1234":
+        '''return f"Bienvenue {user}, vous êtes connecté."'''
+        return render_template("home.html", name_user=user)
+    else:
+        '''return "Une erreur est survenue"'''
+        return render_template("login.html")
+    '''Peut return sur une nouvelle page html potentiellement'''
+
+if __name__ == '__main__':
+    app.run(debug=True)
