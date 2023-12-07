8aca74dbe3e9780ca61b8a3ef479a5f1ce46dc84
Mathys.Rosinski <Mathys.Rosinski@gmail.com>
2023-12-07 12:30:59 +0100

    Migrating SQL request form to adminpanel & CSS tweaks & Adding attributes to Users table

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
@@ -11,6 +11,14 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200),nullable=False)
     password = db.Column(db.String(200),nullable=False)
+    """cat1 = db.Column(db.String(20),nullable=False)
+    cat2 = db.Column(db.String(20),nullable=False)
+    cat3 = db.Column(db.String(20),nullable=False)
+    privilege = db.Column(db.String(10),nullable=False)"""
+    cat1 = db.Column(db.String(20))
+    cat2 = db.Column(db.String(20))
+    cat3 = db.Column(db.String(20))
+    privilege = db.Column(db.String(10))
 
     def __repr__(self):
         return '<Name %r>' %self.id
@@ -20,33 +28,43 @@ class Categories(db.Model):
     category = db.Column(db.String(25),nullable=False)
     link = db.Column(db.String(50),nullable=False)
     title = db.Column(db.String(255),nullable=False)
-    rating = db.Column(db.Integer)
+    rating = db.Column(db.Integer,nullable=False)
 
-"""@app.route("/")
+@app.route("/")
 def bienvenue():
     #A modifier pour push dans le main -> remettre home.html
     return render_template("accueil.html")
 
 @app.route("/adminpanel",methods=['POST','GET'])
-
-
 def adminpanel():
-    if request.method =="POST":
-        #Creating new user from HTML form
-        new_user = Users(name=request.form['username'],password=request.form['password']) # type: ignore
+    if request.method == 'POST' and request.form['action'] == 'add_user':
+        # Creating new user from HTML form
+        new_user = Users(name=request.form['username'], password=request.form['password']) #type:ignore
 
-        #Push to db
+        # Push to db
         try:
             db.session.add(new_user)
             db.session.commit()
             return redirect('/adminpanel')
         except:
             return "Error"
-
     else:
         userlist = Users.query.order_by(Users.id)
-        return render_template("adminpanel.html",userlist=userlist)
+        if request.method == 'POST' and request.form['action'] == 'execute_query':
+            user_input = request.form['sql_query']
+            result = db.session.execute(text(user_input)) #type:ignore
+            
+            result = [row[0] for row in result]
 
+            # If not requested from the adminpanel page, formating result for better compatibility
+            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+                return jsonify(result) 
+            
+            # Process the result or send it to the template
+            return render_template('adminpanel.html', result=result,userlist=userlist)
+        return render_template("adminpanel.html", userlist=userlist)
+
+        
 @app.route("/login")
 def login():
     return render_template("login.html")
@@ -69,27 +87,7 @@ def traitement():
         '''return "Une erreur est survenue"'''
         return render_template("login.html")
     '''Peut return sur une nouvelle page html potentiellement'''
-"""
-@app.route('/')
-def index():
-    return render_template('execute_query.html')  # Render the execute_query form
 
-@app.route('/execute_query', methods=['POST'])
-def execute_query():
-    user_input = request.form.get('sql_query')
-    
-    # Use SQLAlchemy ORM to perform safe database operations
-    result = db.session.execute(text(user_input)) #type:ignore
-
-    names = [row[0] for row in result]
-
-    print("Data in 'names' :",names)
-
-    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        return jsonify(names)
-    
-    # Process the result or send it to the template
-    return render_template('result.html', result=names)
 
 if __name__ == '__main__':
     app.run(debug=True)
