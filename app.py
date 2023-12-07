4696dca4969286d706a583aad7c6459fe7937c48
Mathys.Rosinski <Mathys.Rosinski@gmail.com>
2023-12-08 00:41:55 +0100

    Display & Forms for Users and Videos in Admin Panel

--- app.py
+++ app.py
@@ -11,24 +11,20 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200),nullable=False)
     password = db.Column(db.String(200),nullable=False)
-    """cat1 = db.Column(db.String(20),nullable=False)
-    cat2 = db.Column(db.String(20),nullable=False)
-    cat3 = db.Column(db.String(20),nullable=False)
-    privilege = db.Column(db.String(10),nullable=False)"""
     cat1 = db.Column(db.String(20))
     cat2 = db.Column(db.String(20))
     cat3 = db.Column(db.String(20))
-    privilege = db.Column(db.String(10))
+    privilege = db.Column(db.String(10),nullable=False)
 
     def __repr__(self):
         return '<Name %r>' %self.id
 
-class Categories(db.Model):
+class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     category = db.Column(db.String(25),nullable=False)
     link = db.Column(db.String(50),nullable=False)
     title = db.Column(db.String(255),nullable=False)
-    rating = db.Column(db.Integer,nullable=False)
+    rating = db.Column(db.Integer)
 
 @app.route("/")
 def bienvenue():
@@ -37,9 +33,17 @@ def bienvenue():
 
 @app.route("/adminpanel",methods=['POST','GET'])
 def adminpanel():
+
+    #Add User
     if request.method == 'POST' and request.form['action'] == 'add_user':
         # Creating new user from HTML form
-        new_user = Users(name=request.form['username'], password=request.form['password']) #type:ignore
+        new_user = Users(
+            name=request.form['username'],
+            password=request.form['password'],
+            cat1=request.form['cat1'],
+            cat2=request.form['cat2'],
+            cat3=request.form['cat3'],
+            privilege=request.form['privilege']) #type:ignore
 
         # Push to db
         try:
@@ -47,22 +51,59 @@ def adminpanel():
             db.session.commit()
             return redirect('/adminpanel')
         except:
-            return "Error"
-    else:
-        userlist = Users.query.order_by(Users.id)
-        if request.method == 'POST' and request.form['action'] == 'execute_query':
-            user_input = request.form['sql_query']
-            result = db.session.execute(text(user_input)) #type:ignore
-            
-            result = [row[0] for row in result]
+            return "Error Adding User to DB"
+        
+    #Delete User
+    elif request.method == 'POST' and request.form['action'] =='delete_user':
+        try:
+            db.session.delete(Users.query.get(request.form['userid']))
+            db.session.commit()
+            return redirect('/adminpanel')
+        except:
+            return "Error, no User is associated with this ID."
+        
+    #Add Video
+    elif request.method == 'POST' and request.form['action'] == 'add_video':
+        # Creating new video from HTML form
+        """ MODIFIER POUR INITIALISER LA VALEUR DU RATING A 0 """
+        new_video = Video(
+            category=request.form['category'],
+            link=request.form['link'],
+            title=request.form['title']) #type:ignore
 
-            # If not requested from the adminpanel page, formating result for better compatibility
-            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-                return jsonify(result) 
-            
-            # Process the result or send it to the template
-            return render_template('adminpanel.html', result=result,userlist=userlist)
-        return render_template("adminpanel.html", userlist=userlist)
+        # Push to db
+        try:
+            db.session.add(new_video)
+            db.session.commit()
+            return redirect('/adminpanel')
+        except:
+            return "Error Adding Video to DB"
+        
+    #Delete Video
+    elif request.method == 'POST' and request.form['action'] =='delete_video':
+        try:
+            db.session.delete(Video.query.get(request.form['videoid']))
+            db.session.commit()
+            return redirect('/adminpanel')
+        except:
+            return "Error, no Video is associated with this ID."
+        
+    #SQL QUERY
+    elif request.method == 'POST' and request.form['action'] == 'execute_query':
+        user_input = request.form['sql_query']
+        result = db.session.execute(text(user_input)) #type:ignore
+        
+        result = [row[0] for row in result]
+
+        # If not requested from the adminpanel page, formating result for better compatibility
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            return jsonify(result) 
+        
+        # Process the result or send it to the template
+        return render_template('adminpanel.html', result=result)
+    userlist = Users.query.order_by(Users.id)
+    vidlist = Video.query.order_by(Video.id)
+    return render_template("adminpanel.html", userlist=userlist,vidlist=vidlist)
 
         
 @app.route("/login")
