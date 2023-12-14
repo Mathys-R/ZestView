01286c1926002eaaccc99d4d248696ba0dda6cd6
GwenafyMrg <gwenafy@gmail.com>
2023-12-14 12:25:20 +0100

    Add security adminpanel

--- App.py
+++ App.py
@@ -31,117 +31,123 @@ def bienvenue():
 @app.route("/adminpanel",methods=['POST','GET'])
 def adminpanel():
 
-    error_message = ""
-    #Add User
-    if request.method == 'POST' and request.form['action'] == 'add_user':
+    try :
+        if 'privilege' not in session or session['privilege'] != 'Admin':
+            # Si la session n'existe pas ou si le privilège n'est pas 'Admin', renvoie un message d'erreur
+            return "Accès refusé : privilèges insuffisants."
+        #Add User
+        if request.method == 'POST' and request.form['action'] == 'add_user':
 
-        username_exists = Users.query.filter_by(username=request.form['username']).first()
-        if username_exists:
-            error_message = "Ce nom d'utilisateur existe déjà."
-            userlist = Users.query.order_by(Users.id)
-            vidlist = Video.query.order_by(Video.id)
-            return render_template('adminpanel.html',error_add_user=error_message,userlist=userlist,vidlist=vidlist)
-        else:
-            # Creating new user from HTML form
-            new_user = Users(
-                username=request.form['username'],
-                password=request.form['password'],
-                cat1=request.form['cat1'],
-                cat2=request.form['cat2'],
-                cat3=request.form['cat3'],
-                privilege=request.form['privilege']) #type:ignore
+            username_exists = Users.query.filter_by(username=request.form['username']).first()
+            if username_exists:
+                error_message = "Ce nom d'utilisateur existe déjà."
+                userlist = Users.query.order_by(Users.id)
+                vidlist = Video.query.order_by(Video.id)
+                return render_template('adminpanel.html',error_add_user=error_message,userlist=userlist,vidlist=vidlist)
+            else:
+                # Creating new user from HTML form
+                new_user = Users(
+                    username=request.form['username'],
+                    password=request.form['password'],
+                    cat1=request.form['cat1'],
+                    cat2=request.form['cat2'],
+                    cat3=request.form['cat3'],
+                    privilege=request.form['privilege']) #type:ignore
+                
+                # Push to db
+                try:
+                    db.session.add(new_user)
+                    db.session.commit()
+                    return redirect('/adminpanel')
+                except:
+                    return "Error Adding User to DB"
             
-            # Push to db
+        #Update User Data
+        elif request.method == 'POST' and request.form['action'] == 'update_user_data':
             try:
-                db.session.add(new_user)
+                record = Users.query.get(request.form['userid'])
+                record.username = request.form['username']#type: ignore
+                record.password=request.form['password']#type: ignore
+                record.privilege=request.form['privilege']#type: ignore
                 db.session.commit()
                 return redirect('/adminpanel')
             except:
-                return "Error Adding User to DB"
-        
-    #Update User Data
-    elif request.method == 'POST' and request.form['action'] == 'update_user_data':
-        try:
-            record = Users.query.get(request.form['userid'])
-            record.username = request.form['username']#type: ignore
-            record.password=request.form['password']#type: ignore
-            record.privilege=request.form['privilege']#type: ignore
-            db.session.commit()
-            return redirect('/adminpanel')
-        except:
-            return "Error updating Data"
-        
-    #Delete User
-    elif request.method == 'POST' and request.form['action'] =='delete_user':
-        try:
-            db.session.delete(Users.query.get(request.form['userid']))
-            db.session.commit()
-            return redirect('/adminpanel')
-        except:
-            return "Error, no User is associated with this ID."
-        
-    #Add Video
-    elif request.method == 'POST' and request.form['action'] == 'add_video':
-
-        title_exists = Video.query.filter_by(title=request.form['title']).first()
-        if title_exists:
-            error_message = "Ce titre de vidéo existe déjà."
-            userlist = Users.query.order_by(Users.id)
-            vidlist = Video.query.order_by(Video.id)
-            return render_template('adminpanel.html',error_add_video=error_message,userlist=userlist,vidlist=vidlist)
-        else :  
-            # Creating new video from HTML form
-            """ MODIFIER POUR INITIALISER LA VALEUR DU RATING A 0 """
-            new_video = Video(
-                category=request.form['category'],
-                link=request.form['link'],
-                title=request.form['title']) #type:ignore
-
-            # Push to db
+                return "Error updating Data"
+            
+        #Delete User
+        elif request.method == 'POST' and request.form['action'] =='delete_user':
             try:
-                db.session.add(new_video)
+                db.session.delete(Users.query.get(request.form['userid']))
                 db.session.commit()
                 return redirect('/adminpanel')
             except:
-                return "Error Adding Video to DB"
-        
-    #Update Video Data
-    elif request.method == 'POST' and request.form['action'] == 'update_video_data':
-        try:
-            record = Video.query.get(request.form['videoid'])
-            record.category=request.form['category'] #type:ignore
-            record.link=request.form['link'] #type:ignore
-            record.title=request.form['title'] #type:ignore
-            db.session.commit()
-            return redirect ('/adminpanel')
-        except:
-            return "Error updating Video Data"
+                return "Error, no User is associated with this ID."
+            
+        #Add Video
+        elif request.method == 'POST' and request.form['action'] == 'add_video':
 
-    #Delete Video
-    elif request.method == 'POST' and request.form['action'] =='delete_video':
-        try:
-            db.session.delete(Video.query.get(request.form['videoid']))
-            db.session.commit()
-            return redirect('/adminpanel')
-        except:
-            return "Error, no Video is associated with this ID."
-        
-    #SQL QUERY
-    elif request.method == 'POST' and request.form['action'] == 'execute_query':
-        user_input = request.form['sql_query']
-        result = db.session.execute(text(user_input)) #type:ignore
-        
-        result = [row[0] for row in result]
+            title_exists = Video.query.filter_by(title=request.form['title']).first()
+            if title_exists:
+                error_message = "Ce titre de vidéo existe déjà."
+                userlist = Users.query.order_by(Users.id)
+                vidlist = Video.query.order_by(Video.id)
+                return render_template('adminpanel.html',error_add_video=error_message,userlist=userlist,vidlist=vidlist)
+            else :  
+                # Creating new video from HTML form
+                """ MODIFIER POUR INITIALISER LA VALEUR DU RATING A 0 """
+                new_video = Video(
+                    category=request.form['category'],
+                    link=request.form['link'],
+                    title=request.form['title']) #type:ignore
 
-        # If not requested from the adminpanel page, formating result for better compatibility
-        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-            return jsonify(result) 
+                # Push to db
+                try:
+                    db.session.add(new_video)
+                    db.session.commit()
+                    return redirect('/adminpanel')
+                except:
+                    return "Error Adding Video to DB"
+            
+        #Update Video Data
+        elif request.method == 'POST' and request.form['action'] == 'update_video_data':
+            try:
+                record = Video.query.get(request.form['videoid'])
+                record.category=request.form['category'] #type:ignore
+                record.link=request.form['link'] #type:ignore
+                record.title=request.form['title'] #type:ignore
+                db.session.commit()
+                return redirect ('/adminpanel')
+            except:
+                return "Error updating Video Data"
+
+        #Delete Video
+        elif request.method == 'POST' and request.form['action'] =='delete_video':
+            try:
+                db.session.delete(Video.query.get(request.form['videoid']))
+                db.session.commit()
+                return redirect('/adminpanel')
+            except:
+                return "Error, no Video is associated with this ID."
+            
+        #SQL QUERY
+        elif request.method == 'POST' and request.form['action'] == 'execute_query':
+            user_input = request.form['sql_query']
+            result = db.session.execute(text(user_input)) #type:ignore
+            
+            result = [row[0] for row in result]
+
+            # If not requested from the adminpanel page, formating result for better compatibility
+            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+                return jsonify(result) 
+            
+            # Process the result or send it to the template
+            return render_template('adminpanel.html', result=result)
+        userlist = Users.query.order_by(Users.id)
+        vidlist = Video.query.order_by(Video.id)
+        return render_template("adminpanel.html", userlist=userlist,vidlist=vidlist)
+    except Exception as error:
+        return f"Une erreur s'est produite : {str(error)}"
         
-        # Process the result or send it to the template
-        return render_template('adminpanel.html', result=result)
-    userlist = Users.query.order_by(Users.id)
-    vidlist = Video.query.order_by(Video.id)
-    return render_template("adminpanel.html", userlist=userlist,vidlist=vidlist)
 
 @app.route("/create_account", methods=["GET", "POST"])
 def create_account():
